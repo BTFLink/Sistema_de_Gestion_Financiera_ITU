c59b0492f72bdf62cf8670ca5892f02704256e1d
BTFLink <92482229+BTFLink@users.noreply.github.com>
2024-10-15 08:32:15 -0300

    Initial commit

---
+++ src/main/java/poo/itu/sistema_de_gestion_financiera_itu/Sistema_de_Gestion_Financiera_ITU.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.itu.sistema_de_gestion_financiera_itu;
+
+/**
+ *
+ * @author BTF
+ */
+public class Sistema_de_Gestion_Financiera_ITU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
